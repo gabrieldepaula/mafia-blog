df274bf4e8e50d74b1be48fdb1e3f0dc8efe0866
Gabriel Leite <gabrielleite@luego.com.br>
2021-03-10 19:12:22 -0300

    Working...

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
+import Link from 'next/link';
+
 import { useState } from 'react';
 
 function Home() {
     return (
         <div>
-            <h1>Home 2</h1>
-            <Contador />
+            <h1>Home</h1>
+            {/* <Contador /> */}
+            <Link href="/sobre">
+                <a>Sobre</a>
+            </Link>
         </div>
     );
 }
